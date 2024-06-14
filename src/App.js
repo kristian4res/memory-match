a474afc046eaad6e877f7a7c8a5e7df2837316ee
Kristian T <57638182+kristian4res@users.noreply.github.com>
2024-06-14 14:03:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function App() {
           ))
         }
       </GameGrid>
-      <div style="{{" display:="" 1,="" justify-content:="" center}}=""> 
+      <div style={{ display: "flex", "justify-content": "center"}}> 
         Made by Kristian
       </div>
     </div>
